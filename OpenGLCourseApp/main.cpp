4ddd3384d417de70a793a649b0daf3f22ce2fb41
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-01-08 18:15:47 +0100

    Transform - Scale

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -36,17 +36,22 @@ float triIncrement = 0.005f;
 
 float curAngle = 0.f;
 
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
+
 // Vertex Shader
-static const char* vShader = "														\n\
-# version 330																		\n\
-																					\n\
-layout (location = 0) in vec3 pos;													\n\
-																					\n\
-uniform mat4 model;																	\n\
-																					\n\
-void main()																			\n\
-{																					\n\
-	gl_Position = model * vec4(0.4*pos.x, 0.4*pos.y, pos.z, 1.0); 					\n\
+static const char* vShader = "												\n\
+# version 330																\n\
+																			\n\
+layout (location = 0) in vec3 pos;											\n\
+																			\n\
+uniform mat4 model;															\n\
+																			\n\
+void main()																	\n\
+{																			\n\
+	gl_Position = model * vec4(pos, 1.0); 									\n\
 }";
 
 // Fragment Shader
@@ -226,6 +231,15 @@ int main()
 		curAngle += 0.1f;
 		if (curAngle >= 360) curAngle -= 360;
 
+
+		if (sizeDirection)
+			curSize += 0.001f;
+		else
+			curSize -= 0.001f;
+
+		if (curSize >= maxSize || curSize <= minSize)
+			sizeDirection = !sizeDirection;
+
 		// Clear window
 		glClearColor(0.f, 0.f, 0.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -235,6 +249,7 @@ int main()
 		glm::mat4 model = glm::mat4(1.f);
 		model = glm::translate(model, glm::vec3(triOffset, 0.f, 0.f));
 		model = glm::rotate(model, curAngle * TO_RADIANS, glm::vec3(0.f, 0.f, 1.f));
+		model = glm::scale(model, glm::vec3(curSize, curSize, 1.f));
 				
 		// Params: model location; amount; transpose; pointer to the value;
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
